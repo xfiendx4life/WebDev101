b8818c3d05e9dc5a39019c9662790d5a5e911592
Kirill Ivanov <xfiendx4life@gmail.com>
2023-01-14 13:47:42 +0200

    feat: reviews

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,41 @@
-'use strict';
+"use strict";
 
-function count() {
-    const a = +document.getElementById('a').value;
-    const b = +document.getElementById('b').value;
-    alert(a + b);
+var reviews = new Array();
+
+function sendFeedback() {
+    const name = document.getElementById("name-input");
+    const fBack = document.getElementById("review");
+
+    const nameData = name.value;
+    const fBackValue = fBack.value;
+
+    const date = new Date()
+    reviews.push([nameData, fBackValue, `${date.getDate()}.${date.getMonth() + 1} ${date.getHours()}:${date.getMinutes()}`]);
+    const divFback = document.getElementById("feedback");
+    divFback.innerHTML = "";
+
+    for (let item of reviews) {
+        const fBackBlock = document.createElement("div");
+        fBackBlock.className = "feedback-block";
+
+        const nameDiv = document.createElement("div");
+        nameDiv.className = "name-div";
+        // 
+        nameDiv.innerHTML = `${item[0]} ${item[2]}`;
+
+        const reviewDiv = document.createElement("div");
+        reviewDiv.className = "review-value";
+        reviewDiv.innerHTML = item[1];
+
+        fBackBlock.appendChild(nameDiv);
+        fBackBlock.appendChild(reviewDiv);
+
+        divFback.appendChild(fBackBlock);
+
+    }
 }
+
+addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("send-feedback");
+    btn.onclick = sendFeedback;
+});

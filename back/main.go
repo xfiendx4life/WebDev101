09a64742bcd5db1f9e6c63a8407cd6c8e5f14485
Kirill Ivanov <xfiendx4life@gmail.com>
2023-02-18 15:19:15 +0200

    feat: web service example

--- back/main.go
+++ back/main.go
@@ -1,67 +1,81 @@
 package main
 
-func abs(a int) int {
-	if a > 0 {
-		return a
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type AddTimePayload struct {
+	Years   int `json:"years,omitempty"`
+	Months  int `json:"months,omitempty"`
+	Days    int `json:"days,omitempty"`
+	Hours   int `json:"hours,omitempty"`
+	Minutes int `json:"minutes,omitempty"`
+}
+
+// accept arg period and possible values hour and second
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	res := r.URL.Query()
+	nw := time.Now()
+	var toWrite string
+	if val := res.Get("period"); val != "" {
+		switch val {
+		case "hour":
+			toWrite = "HOUR: " + strconv.Itoa(nw.Hour())
+		case "minute":
+			toWrite = "MINUTE: " + strconv.Itoa(nw.Minute())
+		default:
+			toWrite = nw.Format("01/02/2006 15:04")
+		}
+	} else {
+		toWrite = nw.Format("01/02/2006 15:04")
 	}
-	return -a
+
+	fmt.Fprint(w, toWrite)
+}
+
+// Accepts JSON via POST method
+// format: {years: int, months: int, days: int, hours: int, minutes: int}
+func AddTimeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Internal Server Error: %s", err)
+			return
+		}
+		var timeToAdd AddTimePayload
+		log.Println(string(data))
+		err = json.Unmarshal(data, &timeToAdd)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Internal Server Error %s", err.Error())
+			return
+		}
+		nw := time.Now()
+		nw = nw.Add(time.Minute * time.Duration(timeToAdd.Years) * 525600)
+		nw = nw.Add(time.Minute * time.Duration(timeToAdd.Months) * 43800)
+		nw = nw.Add(time.Minute * time.Duration(timeToAdd.Days) * 24 * 60)
+		nw = nw.Add(time.Minute * time.Duration(timeToAdd.Hours) * 60)
+		nw = nw.Add(time.Minute * time.Duration(timeToAdd.Minutes))
+
+		fmt.Fprintf(w, "In %d years and %d months and %d weeks and %d hours and %d minutes the time is: %s",
+			timeToAdd.Years, timeToAdd.Months, timeToAdd.Days, timeToAdd.Hours, timeToAdd.Minutes, nw.Format("01/02/2006 15:04"))
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Use json ")
+		return
+	}
 }
 
 func main() {
-	// fmt.Println("Hello, World")
-	// fmt.Printf("Hello, web %T\n", 101)
-	// var a int
-	// a = 10
-	// fmt.Println(a)
-	// b := 4
-	// fmt.Printf("%d %T\n", b, b)
-	// fmt.Println(a + b)
-	// fmt.Println(a*b, a/b, a%b, float64(a)/float64(b), a-b)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/add_time", AddTimeHandler)
 
-	// s := "хello"
-	// fmt.Printf("len = %d, rune count = %d\n", len(s), utf8.RuneCountInString(s))
-	// r := 'х'
-	// fmt.Println(r)
-	// fmt.Println(s[3])
-	// for i := 0; i < utf8.RuneCountInString(s); i++ {
-	// 	if i == 3 {
-	// 		continue
-	// 	}
-	// 	fmt.Println(s[i])
-	// }
-	// a = 123
-	// sum := 0
-	// for sum = 0; a > 0; a /= 10 {
-	// 	sum += a % 10
-	// }
-	// fmt.Println(sum)
-
-	// if sum > 10 {
-	// 	fmt.Println("sum > 10")
-	// } else if a < 5 {
-	// 	fmt.Println("hello")
-	// }
-
-	// rand.Seed(time.Now().Unix())
-	// a = rand.Intn(11)
-	// switch a {
-	// case 5:
-	// 	fmt.Println("In the middle")
-	// case 10:
-	// 	fmt.Println("impossible")
-	// default:
-	// 	fmt.Printf("something usual %d\n", a)
-	// }
-
-	// _, err  := fmt.Scan(&a)
-	// a = 5
-	// if err != nil {
-	// 	fmt.Printf("error accured %s\n", err)
-	// }
-	// var arr []int
-	// fmt.Println(arr[3])
-
-	// second()
-	third()
-
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
